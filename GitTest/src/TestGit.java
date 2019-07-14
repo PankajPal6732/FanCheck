552afe0294d87a0ff5a7e97556ca5720b74d4a63
pankaj6732 <pankaj6732@gmail.com>
2019-07-14 14:13:21 +0530

    change to Localtime

--- GitTest/src/TestGit.java
+++ GitTest/src/TestGit.java
@@ -1,6 +1,9 @@
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("hello git u have uncommit");
+		System.out.println(Date.valueOf(LocalDate.now()));
 	}
 }
